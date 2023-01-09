39235485a2167af32dad69f0b4f11355e2e72e64
Franco Tejada <frante.2717@gmail.com>
2023-01-09 17:45:44 -0300

    navbar y hero parte react

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,6 +1,45 @@
 import React, { useEffect, useState } from "react";
+import logo from "../../img/logo.png";
 import "./header.css";
 
 export default function Header() {
+  return (
+    <header>
+      <div className="contenainer_header">
+        <img src={logo} alt=""/>
+        <nav id="nav" className="">
+          <ul id="links">
+            <li>
+              <a href="#inicio" className="active">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#tecnologias">Tecnologias</a>
+            </li>
+            <li>
+              <a href="#proyectos">Proyectos</a>
+            </li>
+            <li>
+              <a href="#sobremi">Sobre Mi</a>
+            </li>
+            <li>
+              <a href="#contacto">Contacto</a>
+            </li>
+          </ul>
+        </nav>
 
+        {/* Iconos del menu resposive */}
+        <div id="icono-nav">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+        
+        {/* Iconos de las redes sociales */}
+        <div id="redes">
+          <a href><i class="fa-brands fa-github"></i></a>
+          <a href><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,5 +3,10 @@ import "./Hero.css";
 
 
 export default function Hero() {
-
+  return(
+    <div className="hero">
+      <p className="description">Hola</p>
+      <h2>Soy Franco Tejada, Desarrollador Full Stack</h2>
+    </div>
+  )
 }
